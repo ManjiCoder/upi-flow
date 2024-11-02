d31e063bc2d4a8a97dd1e4731d14617e053cb03a
Manji Coder <manjicoder@gmail.com>
2024-11-02 18:39:42 +0530

    credit debit fix

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -15,11 +15,11 @@ export default function Settings() {
     // });
     // console.table(temp);
     if (rows) {
-      // console.table(
-      //   rows.map(({ credit, debit, balance, date, receiver }) => {
-      //     return { date, balance, credit, debit, receiver };
-      //   })
-      // );
+      console.table(
+        rows.map(({ credit, debit, balance, date, receiver }) => {
+          return { date, balance, credit, debit, receiver };
+        })
+      );
     }
   }, []);
 
