e90c5990a0feda536d8d5174e81db30b51de711c
Manji Coder <manjicoder@gmail.com>
2025-02-16 15:03:41 +0530

    frequentTranscation Table

--- src/components/FrequentTranscactionTable.tsx
+++ src/components/FrequentTranscactionTable.tsx
@@ -1,42 +1,241 @@
 import {
+  ColumnDef,
+  SortingState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
+import { ArrowUpDown, MoreHorizontal } from 'lucide-react';
+import * as React from 'react';
+
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
-import { useAppSelector } from '@/redux/hooks';
-import { frequentTranscation } from '@/utils/helper';
+import { formattedAmount } from '@/utils/helper';
 
-export default function FrequentTranscactionTable() {
-  const { filterData } = useAppSelector((state) => state.dateSlice);
-  const records = frequentTranscation(Object.values(filterData).flat()).sort(
-    (a, b) => b.count - a.count
-  );
-  console.log(records);
+export type Payment = {
+  name: string;
+  count: number;
+  credit: number;
+  debit: number;
+};
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: 'name',
+    header: 'Name',
+    cell: ({ row }) => <div className='capitalize'>{row.getValue('name')}</div>,
+  },
+  {
+    accessorKey: 'count',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          className='w-full'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Count
+          <ArrowUpDown />
+        </Button>
+      );
+    },
+    cell: ({ row }) => (
+      <div className='text-center'>{row.getValue('count')}</div>
+    ),
+  },
+  {
+    accessorKey: 'credit',
+    header: ({ column }) => (
+      <Button
+        variant='ghost'
+        className='w-full'
+        onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+      >
+        Credit
+        <ArrowUpDown />
+      </Button>
+    ),
+    cell: ({ row }) => {
+      return (
+        <div className='text-center font-medium text-green-600 dark:text-green-400'>
+          {row.getValue('credit') == 0
+            ? ''
+            : `+${formattedAmount(row.getValue('credit'), true)}`}
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: 'debit',
+    header: ({ column }) => (
+      <Button
+        variant='ghost'
+        className='w-full'
+        onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+      >
+        Debit
+        <ArrowUpDown />
+      </Button>
+    ),
+    cell: ({ row }) => {
+      return (
+        <div className='text-center font-medium text-red-600 dark:text-red-400 '>
+          {row.getValue('debit') == 0
+            ? ''
+            : `-${formattedAmount(row.getValue('debit'), true)}`}
+        </div>
+      );
+    },
+  },
+  {
+    id: 'actions',
+    header: 'Action',
+    enableHiding: false,
+    cell: ({ row }) => {
+      const payment = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant='ghost' className='h-8 w-8 p-0'>
+              <span className='sr-only'>Open menu</span>
+              <MoreHorizontal />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align='end'>
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(payment.name)}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
+
+export default function FrequentTranscactionTable({
+  records,
+}: {
+  records: Payment[];
+}) {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [rowSelection, setRowSelection] = React.useState({});
+
+  const table = useReactTable({
+    data: records,
+    columns,
+    onSortingChange: setSorting,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      rowSelection,
+    },
+  });
+
   return (
-    <Table className='overflow-hidden'>
-      <TableCaption>A list of Frequent Transcation.</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className=''>Receiver</TableHead>
-          <TableHead>Count</TableHead>
-          <TableHead>Credit</TableHead>
-          <TableHead className='text-right'>Debit</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {records.map((item) => (
-          <TableRow key={item.name}>
-            <TableCell className='font-medium'>{item.name}</TableCell>
-            <TableCell>{item.count}</TableCell>
-            <TableCell>{item.credit || ''}</TableCell>
-            <TableCell className='text-right'>{item.debit || ''}</TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <div className='w-full'>
+      <div className='rounded-md border'>
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && 'selected'}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className='h-24 text-center'
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className='flex items-center justify-end space-x-2 py-4'>
+        <div className='flex-1 text-sm text-muted-foreground'>
+          {table.getFilteredSelectedRowModel().rows.length} of{' '}
+          {table.getFilteredRowModel().rows.length} row(s) selected.
+        </div>
+        <div className='space-x-2'>
+          <Button
+            variant='outline'
+            size='sm'
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant='outline'
+            size='sm'
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/pages/Analytics.tsx
+++ src/pages/Analytics.tsx
@@ -2,13 +2,19 @@ import FlowInfo from '@/components/FlowInfo';
 import FrequentTranscactionTable from '@/components/FrequentTranscactionTable';
 import PageWrapper from '@/components/layout/PageWrapper';
 import { LineGraph } from '@/components/LineGraph';
+import { useAppSelector } from '@/redux/hooks';
+import { frequentTranscation } from '@/utils/helper';
 
 export default function Analytics() {
+  const { filterData } = useAppSelector((state) => state.dateSlice);
+  const records = frequentTranscation(Object.values(filterData).flat()).sort(
+    (a, b) => b.count - a.count
+  );
   return (
     <PageWrapper className='pt-0'>
       <FlowInfo />
       <LineGraph />
-      <FrequentTranscactionTable />
+      <FrequentTranscactionTable records={records} />
     </PageWrapper>
   );
 }
